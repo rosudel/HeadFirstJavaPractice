d2bf9b98e4fb148c2e938d8b9ca117e09c67ff96
rosudel <67065387+rosudel@users.noreply.github.com>
2022-08-14 02:52:24 +0600

    Delete StreamingSongTestDrive.java

--- StreamingSongTestDrive.java
+++
@@ -1,9 +0,0 @@
-class StreamingSongTestDrive {
-public static void main(String[] args){
-    StreamingSong obj = new StreamingSong();
-    obj.artist = "The Beatles";
-    obj.title = "Come Together";
-    obj.play();
-    obj.printDetails();
-}
-}
