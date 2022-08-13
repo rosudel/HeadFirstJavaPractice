2bae4dd91ade50c438c8cd33d1cfd9be56316e90
rosudel <rosudel3@gmail.com>
2022-08-14 02:49:35 +0600

    exercise

---
+++ Episode.java
@@ -0,0 +1,10 @@
+class Episode {
+    int seriesNumber;
+    int episodeNumber;
+    void skipIntro() {
+        System.out.println("Skipping intro...");
+    }
+    void skipToNext() {
+        System.out.println("Loading next episode...");
+    }
+}
---
+++ StreamingSongTestDrive.java
@@ -0,0 +1,9 @@
+class StreamingSongTestDrive {
+public static void main(String[] args){
+    StreamingSong obj = new StreamingSong();
+    obj.artist = "The Beatles";
+    obj.title = "Come Together";
+    obj.play();
+    obj.printDetails();
+}
+}
