97841353d36cf164dd7effbb51d2acb000089ecd
rosudel <67065387+rosudel@users.noreply.github.com>
2022-08-14 02:53:06 +0600

    Delete EpisodeTestDrive.java

--- EpisodeTestDrive.java
+++
@@ -1,8 +0,0 @@
-class EpisodeTestDrive {
-public static void main(String[] args) {
-        Episode episode = new Episode();
-        episode.seriesNumber = 4;
-        episode.skipToNext();
-        episode.skipIntro();
-        }
-}
