0a2f2eaa9e5ec440eb8950294506c55e3e287fc4
rosudel <rosudel3@gmail.com>
2022-08-16 19:48:43 +0600

    added

---
+++ ClockTestDrive.java
@@ -0,0 +1,10 @@
+//page 277
+
+class ClockTestDrive {
+public static void main(String[] args) {
+        Clock c = new Clock();
+        c.setTime("1245");
+        String tod = c.getTime();
+        System.out.println("time: "+tod);
+        }
+        }
